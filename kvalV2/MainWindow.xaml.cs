9d946722f448596459583fcf328d35c25c56954d
Мария Домбровская <marik762002@gmail.com>
2021-03-22 16:05:05 +0300

    окно авторизации

--- kvalV2/MainWindow.xaml.cs
+++ kvalV2/MainWindow.xaml.cs
@@ -24,5 +24,29 @@ namespace kvalV2
         {
             InitializeComponent();
         }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Entrance_Click(object sender, RoutedEventArgs e)
+        {
+            Error.Text = "";
+            if ((Login.Text == "") && (Password.Text == ""))
+            {
+                Error.Text = "Вы ничего не ввели ";
+            }
+            else
+            if (Login.Text == "") 
+            {
+                Error.Text = "Вы не ввели логин";
+            } 
+            else
+                if (Password.Text == "")
+            {
+                Error.Text = "Вы не ввели пароль";
+            }
+        }
     }
 }
